8da66cba0590caaa59c4c9f33b5b4f979d06c178
ALABERTUS <alabertus@gmail.com>
2023-08-25 13:38:39 +0200

    Initial commit

---
+++ src/main/java/com/Lo_tienes_CRUDo/LoTienesCruDoApplication.java
@@ -0,0 +1,13 @@
+package com.Lo_tienes_CRUDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoTienesCruDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoTienesCruDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Lo_tienes_CRUDo/LoTienesCruDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lo_tienes_CRUDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoTienesCruDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
